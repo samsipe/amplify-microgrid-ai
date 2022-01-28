6c987a442f20280104ce5beb308de59c6090dae7
Sam Sipe <sam@sipe.io>
2022-01-28 01:35:33 -0500

    TTWS, remove unused imports

--- amplify/DataGeneration.py
+++ amplify/DataGeneration.py
@@ -1,19 +1,12 @@
-import os
+
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 import datetime as dt
 
 from glob import glob
-from os.path import dirname, abspath, join, exists
 
-from sklearn import linear_model, preprocessing
-from typing import Tuple
-
-# Pysolar
 from pysolar.solar import get_altitude, radiation
-
-# ClearML
 from clearml import Dataset
 
 # TODO: replace prints with logging
@@ -21,16 +14,16 @@ class DataGenerator:
     """
     Generates data for training using weather and power data.
     """
-    
+
     def __init__(self,
                  use_local_data: bool=False,
-                 
+
                  weather_features: list=None,
                  building_features: list=None,
-                 
+
                  building_lat: float=39.9649,
                  building_lon: float=-75.1396,
-                 
+
                  building_data_dir: str=None,
                  weather_data_dir: str=None,
                 ):
@@ -45,7 +38,7 @@ class DataGenerator:
         self.building_features = building_features
         self.building_lat = building_lat
         self.building_lon = building_lon
-        
+
         self.building_data_dir = building_data_dir
         self.weather_data_dir = weather_data_dir
 
@@ -54,25 +47,25 @@ class DataGenerator:
         self.end_date = None
         self.building_data: pd.DataFrame = None  # Building data
         self.weather_data: pd.DataFrame = None  # Weather data
-        
+
         # Check for specified/local building and weather directories. If not specified, use ClearML
-        if not self.building_data_dir: 
+        if not self.building_data_dir:
             try:
                 self.building_data_dir = glob(
                     Dataset.get(
-                        dataset_project="amplify", 
+                        dataset_project="amplify",
                         dataset_name="building_data"
                     ).get_local_copy()
                     + "/**"
                 )[0]
             except:
                 print('No directory was specified and building data could not be retrieved from ClearML')
-                
+
         if not self.weather_data_dir:
             try:
                 self.weather_data_dir = glob(
                     Dataset.get(
-                        dataset_project="amplify", 
+                        dataset_project="amplify",
                         dataset_name="weather_data"
                     ).get_local_copy()
                     + "/**"
@@ -81,16 +74,16 @@ class DataGenerator:
                 print('No directory was specified and weather data could not be retrieved from ClearML')
 
         # Set weather columns to keep
-        self.weather_data_keep_columns = (          
+        self.weather_data_keep_columns = (
             self.weather_features
             if self.weather_features
             else ["temp",
-                  "pressure", 
-                  "humidity", 
+                  "pressure",
+                  "humidity",
                   "clouds_all"
                  ]
         )
-    
+
         # Set building columns to keep
         self.building_data_keep_columns = (
             self.building_features
@@ -107,7 +100,7 @@ class DataGenerator:
                 "Current (A)",
             ]
         )
-        
+
     def load_data(self):
         """
         Loads data for a specified building/weather data paths. If no paths are specified, uses
@@ -126,7 +119,7 @@ class DataGenerator:
             building_data (dataframe)   : building data
             weather_data (dataframe)    : weather data
         """
-        
+
         # Now actually load the data. 1st check that directories are set
         if ((self.weather_data_dir is not None)
             & (self.building_data_dir is not None)
@@ -142,7 +135,7 @@ class DataGenerator:
 
         Return:
             cleaned building data
-        """        
+        """
         try:
             self.building_data = pd.read_csv(
                 self.building_data_dir, header=None, low_memory=False
@@ -195,9 +188,9 @@ class DataGenerator:
 
             # Slice to the two power systems we're monitoring and rename columns
             # TODO: make the power systems vairable at some point
-            self.building_data = self.building_data[["PowerScout DPS126", 
+            self.building_data = self.building_data[["PowerScout DPS126",
                                                      "PowerScout DPS121"]
-                                                   ].rename(columns={"PowerScout DPS126": "solar", 
+                                                   ].rename(columns={"PowerScout DPS126": "solar",
                                                                      "PowerScout DPS121": "usage"
                                                                     }
                                                            )
@@ -207,7 +200,7 @@ class DataGenerator:
 
             # Create DF with only Energy - keep just the last value (meter readings)
             self.building_data = self.building_data.loc[
-                idx[:], 
+                idx[:],
                 idx[:, self.building_data_keep_columns]
             ]
 
@@ -215,7 +208,7 @@ class DataGenerator:
 
         except:
             print("Error: Cannot load building data!")
-            
+
         return self.building_data
 
     def _load_weather_data(self):
@@ -225,12 +218,12 @@ class DataGenerator:
         Return:
             cleaned weather data
         """
-        
+
         try:
             self.weather_data = pd.read_csv(
                 self.weather_data_dir, header=0, low_memory=False
             )
-            
+
             #### pull lat/lon from weather data for irradiance
             #self.building_lat
             #self.building_lon
@@ -246,7 +239,7 @@ class DataGenerator:
 
             # Drop 2nd datetime column that's not needed
             self.weather_data = self.weather_data[self.weather_data_keep_columns]
-            
+
             # deduplicate index
             self.weather_data = self.weather_data.drop_duplicates()
 
@@ -254,38 +247,38 @@ class DataGenerator:
             self.weather_data["irradiance"] = np.nan
             self.date_list = list(self.weather_data.index)
             for date in self.date_list:
-                self.altitude_deg = get_altitude(self.building_lat, 
-                                                 self.building_lon, 
+                self.altitude_deg = get_altitude(self.building_lat,
+                                                 self.building_lon,
                                                  date.to_pydatetime()
                                                 )
-                self.weather_data.loc[date.to_pydatetime(), 
+                self.weather_data.loc[date.to_pydatetime(),
                                       "irradiance"
                 ] = radiation.get_radiation_direct(date.to_pydatetime(),
                                                    self.altitude_deg)
 
             # Fill nulls in irradiance with 0
             self.weather_data = self.weather_data.replace(np.nan, 0)
-            
+
             print("Info: Successfully loaded weather data!")
 
         except:
             print("Error: Cannot load weather data")
-            
+
         return self.weather_data
-            
+
     def _daylight_savings(self):
         """
         With loaded weather data and building data,
         match indexes and adjust for daylight savings.
-        
+
         *NOTE: Forces Building and Weather data loading functions
         to run
-        
+
         Return: merged clean building and weather data.
         """
         self.building_data = self._load_building_data()
         self.weather_data = self._load_weather_data()
-        
+
             # Merge Building Solar Generation Y data to merged_data
         self.merged_data = self.weather_data.merge(
                 self.building_data.solar['True Power (kW)'],
@@ -296,7 +289,7 @@ class DataGenerator:
         self.merged_data = self.merged_data.fillna(method='ffill'
             ).dropna()
         self.merged_data.rename(columns={'True Power (kW)' : 'solar'}, inplace=True)
-            
+
             # Merge Building Usage Y data to merged_data
         self.merged_data = self.merged_data.merge(
                 self.building_data.usage['True Power (kW)'],
@@ -305,5 +298,5 @@ class DataGenerator:
                 right_index=True
             ).fillna(method='ffill')
         self.merged_data.rename(columns={'True Power (kW)' : 'usage'}, inplace=True)
-            
+
         return self.merged_data
