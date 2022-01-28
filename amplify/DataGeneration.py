3071b86be9c17f61fa492ab98ab29b5ec1bdcf76
Sam Sipe <sam@sipe.io>
2022-01-28 01:54:36 -0500

    Blacken DataGen class, dynamic lat/lon pickup

--- amplify/DataGeneration.py
+++ amplify/DataGeneration.py
@@ -1,9 +1,5 @@
-
 import pandas as pd
 import numpy as np
-
-import datetime as dt
-
 from glob import glob
 
 from pysolar.solar import get_altitude, radiation
@@ -15,18 +11,14 @@ class DataGenerator:
     Generates data for training using weather and power data.
     """
 
-    def __init__(self,
-                 use_local_data: bool=False,
-
-                 weather_features: list=None,
-                 building_features: list=None,
-
-                 building_lat: float=39.9649,
-                 building_lon: float=-75.1396,
-
-                 building_data_dir: str=None,
-                 weather_data_dir: str=None,
-                ):
+    def __init__(
+        self,
+        use_local_data: bool = False,
+        weather_features: list = None,
+        building_features: list = None,
+        building_data_dir: str = None,
+        weather_data_dir: str = None,
+    ):
         """
         Initialize data generator.
 
@@ -36,15 +28,10 @@ class DataGenerator:
         self.use_local_data = use_local_data
         self.weather_features = weather_features
         self.building_features = building_features
-        self.building_lat = building_lat
-        self.building_lon = building_lon
-
         self.building_data_dir = building_data_dir
         self.weather_data_dir = weather_data_dir
 
         # Data
-        self.start_date = None
-        self.end_date = None
         self.building_data: pd.DataFrame = None  # Building data
         self.weather_data: pd.DataFrame = None  # Weather data
 
@@ -53,35 +40,33 @@ class DataGenerator:
             try:
                 self.building_data_dir = glob(
                     Dataset.get(
-                        dataset_project="amplify",
-                        dataset_name="building_data"
+                        dataset_project="amplify", dataset_name="building_data"
                     ).get_local_copy()
                     + "/**"
                 )[0]
             except:
-                print('No directory was specified and building data could not be retrieved from ClearML')
+                print(
+                    "No directory was specified and building data could not be retrieved from ClearML"
+                )
 
         if not self.weather_data_dir:
             try:
                 self.weather_data_dir = glob(
                     Dataset.get(
-                        dataset_project="amplify",
-                        dataset_name="weather_data"
+                        dataset_project="amplify", dataset_name="weather_data"
                     ).get_local_copy()
                     + "/**"
                 )[0]
             except:
-                print('No directory was specified and weather data could not be retrieved from ClearML')
+                print(
+                    "No directory was specified and weather data could not be retrieved from ClearML"
+                )
 
         # Set weather columns to keep
         self.weather_data_keep_columns = (
             self.weather_features
             if self.weather_features
-            else ["temp",
-                  "pressure",
-                  "humidity",
-                  "clouds_all"
-                 ]
+            else ["temp", "pressure", "humidity", "clouds_all"]
         )
 
         # Set building columns to keep
@@ -121,11 +106,12 @@ class DataGenerator:
         """
 
         # Now actually load the data. 1st check that directories are set
-        if ((self.weather_data_dir is not None)
+        if (
+            (self.weather_data_dir is not None)
             & (self.building_data_dir is not None)
             & (self.weather_data_keep_columns is not None)
             & (self.building_data_keep_columns is not None)
-           ):
+        ):
             # With data directories set, return the retrieved/cleaned/merged data
             return self._daylight_savings()
 
@@ -171,37 +157,37 @@ class DataGenerator:
             self.building_data.index.rename("Timestamp", inplace=True)
 
             # Set building_data to Eastern timezone and then convert to UTC
-            self.building_data = self.building_data.tz_localize("America/New_York",
-                                                                ambiguous=True
-                                                               ).tz_convert("UTC")
+            self.building_data = self.building_data.tz_localize(
+                "America/New_York", ambiguous=True
+            ).tz_convert("UTC")
 
             # deduplicate index
             self.building_data = self.building_data.drop_duplicates(keep="last")
 
             # Drop any column or row with all nulls
-            self.building_data = self.building_data.dropna(how="all", axis=1
-                                                          ).dropna(how="all", axis=0
-                                                                  )
+            self.building_data = self.building_data.dropna(how="all", axis=1).dropna(
+                how="all", axis=0
+            )
 
             # remove noise (zeros) from the building_data
-            self.building_data = self.building_data.replace(0, np.nan).fillna(method="ffill")
+            self.building_data = self.building_data.replace(0, np.nan).fillna(
+                method="ffill"
+            )
 
             # Slice to the two power systems we're monitoring and rename columns
             # TODO: make the power systems vairable at some point
-            self.building_data = self.building_data[["PowerScout DPS126",
-                                                     "PowerScout DPS121"]
-                                                   ].rename(columns={"PowerScout DPS126": "solar",
-                                                                     "PowerScout DPS121": "usage"
-                                                                    }
-                                                           )
+            self.building_data = self.building_data[
+                ["PowerScout DPS126", "PowerScout DPS121"]
+            ].rename(
+                columns={"PowerScout DPS126": "solar", "PowerScout DPS121": "usage"}
+            )
 
             # Create our separate y-columns: solar pwr generated, buildling pwr used
             idx = pd.IndexSlice
 
             # Create DF with only Energy - keep just the last value (meter readings)
             self.building_data = self.building_data.loc[
-                idx[:],
-                idx[:, self.building_data_keep_columns]
+                idx[:], idx[:, self.building_data_keep_columns]
             ]
 
             print("Info: Successfully loaded building data!")
@@ -225,8 +211,8 @@ class DataGenerator:
             )
 
             #### pull lat/lon from weather data for irradiance
-            #self.building_lat
-            #self.building_lon
+            self.building_lat = self.weather_data.lat.iloc[0]
+            self.building_lon = self.weather_data.lon.iloc[0]
 
             ## Clean up datetime, drop 2nd datetime column
             # Convert from POSIX to ISO UTC time
@@ -247,14 +233,14 @@ class DataGenerator:
             self.weather_data["irradiance"] = np.nan
             self.date_list = list(self.weather_data.index)
             for date in self.date_list:
-                self.altitude_deg = get_altitude(self.building_lat,
-                                                 self.building_lon,
-                                                 date.to_pydatetime()
-                                                )
-                self.weather_data.loc[date.to_pydatetime(),
-                                      "irradiance"
-                ] = radiation.get_radiation_direct(date.to_pydatetime(),
-                                                   self.altitude_deg)
+                self.altitude_deg = get_altitude(
+                    self.building_lat, self.building_lon, date.to_pydatetime()
+                )
+                self.weather_data.loc[
+                    date.to_pydatetime(), "irradiance"
+                ] = radiation.get_radiation_direct(
+                    date.to_pydatetime(), self.altitude_deg
+                )
 
             # Fill nulls in irradiance with 0
             self.weather_data = self.weather_data.replace(np.nan, 0)
@@ -279,24 +265,23 @@ class DataGenerator:
         self.building_data = self._load_building_data()
         self.weather_data = self._load_weather_data()
 
-            # Merge Building Solar Generation Y data to merged_data
+        # Merge Building Solar Generation Y data to merged_data
         self.merged_data = self.weather_data.merge(
-                self.building_data.solar['True Power (kW)'],
-                'outer',
-                left_index=True,
-                right_index=True
+            self.building_data.solar["True Power (kW)"],
+            "outer",
+            left_index=True,
+            right_index=True,
         )
-        self.merged_data = self.merged_data.fillna(method='ffill'
-            ).dropna()
-        self.merged_data.rename(columns={'True Power (kW)' : 'solar'}, inplace=True)
+        self.merged_data = self.merged_data.fillna(method="ffill").dropna()
+        self.merged_data.rename(columns={"True Power (kW)": "solar"}, inplace=True)
 
-            # Merge Building Usage Y data to merged_data
+        # Merge Building Usage Y data to merged_data
         self.merged_data = self.merged_data.merge(
-                self.building_data.usage['True Power (kW)'],
-                'outer',
-                left_index=True,
-                right_index=True
-            ).fillna(method='ffill')
-        self.merged_data.rename(columns={'True Power (kW)' : 'usage'}, inplace=True)
+            self.building_data.usage["True Power (kW)"],
+            "outer",
+            left_index=True,
+            right_index=True,
+        ).fillna(method="ffill")
+        self.merged_data.rename(columns={"True Power (kW)": "usage"}, inplace=True)
 
         return self.merged_data
