1b085542a4a6083694596e4ed8e9900ee5973b87
Christian Welling <csw73@cornell.edu>
2022-01-30 22:53:16 -0800

    add multilayer lstm to models

--- amplify/models.py
+++ amplify/models.py
@@ -8,11 +8,13 @@ from keras.models import Sequential, Model
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Flatten
+from keras.layers import Dropout
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from matplotlib import pyplot
 from keras.layers import Bidirectional
 from keras.layers import TimeDistributed
+
 
 class IModel:
     """
@@ -33,7 +35,7 @@ class IModel:
         """
         if self.model:
             self.model.summary()
-            tf.keras.utils.plot_model(self.model, show_shapes=True, show_layer_names=False)
+            keras.utils.plot_model(self.model, show_shapes=True, show_layer_names=False)
 
     def CreateModel(self):
         """
@@ -68,7 +70,9 @@ class IModel:
         if self.model:
             return self.model.predict(x_test)
 
+
 ### Test Models Below ###
+
 
 class SimpleLSTM_1(IModel):
     """
@@ -109,6 +113,7 @@ class SimpleLSTM_1(IModel):
         # norm_input = Input(shape=(self.n_series_len, self.n_series_ft))
         # seq_input = self.norm_layer(norm_input)
         model = Sequential()
+        model.add(Input(shape=(self.n_series_len, self.n_series_ft)))
         model.add(self.norm_layer)  # , input_shape=(self.n_series_len, self.n_series_ft))
         model.add(
             LSTM(self.n_layer, activation=self.activation_f, input_shape=(self.n_series_len, self.n_series_ft), return_sequences=True)
@@ -146,3 +151,121 @@ class SimpleLSTM_1(IModel):
         )
 
         return self.history
+
+
+class MultiLayerLSTM(IModel):
+    """
+    MultiLayer LSTM Model.
+    """
+
+    def __init__(self, norm_layer, n_layer=1, n_series_len=48, n_series_ft=6, n_series_out=1, activation_f='tanh', dropout_rate=0.25):
+        """ Initialize model """
+        IModel.__init__(self, model_name="MultiLayerLSTM")
+        self.n_layer = n_layer
+        self.n_series_len = n_series_len
+        self.n_series_ft = n_series_ft
+        self.n_series_out = n_series_out
+        self.activation_f = activation_f
+        self.norm_layer = norm_layer
+        self.dropout_rate = dropout_rate
+
+        # hyper param
+        FACTOR = 0.1
+        PATIENCE = 5
+
+        # model callbacks
+        self.model_cp = tf.keras.callbacks.ModelCheckpoint(
+            "../models/multi_layer_lstm_weights.hdf5",
+            monitor="val_loss",
+            mode='min',
+            save_best_only=True,
+            save_weights_only=True,
+            verbose=1,
+        )
+        self.reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
+            monitor='val_loss',
+            mode='min',
+            factor=FACTOR,
+            patience=PATIENCE,
+            min_lr=1e-8,
+            verbose=1,
+        )
+        self.early_stop = tf.keras.callbacks.EarlyStopping(monitor="val_loss", mode='min', patience=(3 * PATIENCE), verbose=1)
+        self.tensorboard_logs = tf.keras.callbacks.TensorBoard('../logs/', histogram_freq=1)
+        self.callbacks = [self.model_cp, self.reduce_lr, self.early_stop, self.tensorboard_logs]
+
+        self.SetHyperParam()
+        self.CreateModel()
+
+    def SetHyperParam(self, epoch=30, batch_size=10):
+        """
+        Set hyperparameters for training
+
+        Arguments:
+            epoch (int)         : training epoch
+            batch_size (int)    : training batch size
+        """
+        self.epoch = epoch
+        self.batch_size = batch_size
+
+    def CreateModel(self):
+        """
+        Create the model
+        """
+        # norm_input = Input(shape=(self.n_series_len, self.n_series_ft))
+        # seq_input = self.norm_layer(norm_input)
+        model = Sequential()
+        model.add(Input(shape=(self.n_series_len, self.n_series_ft)))
+        model.add(self.norm_layer)  # , input_shape=(self.n_series_len, self.n_series_ft))
+        model.add(Dropout(rate=self.dropout_rate))
+        model.add(Dense(self.n_series_ft, activation='relu'))
+        model.add(
+            LSTM(self.n_layer, activation=self.activation_f, input_shape=(self.n_series_len, self.n_series_ft), return_sequences=True)
+        )
+        model.add(TimeDistributed(Dense(self.n_series_out)))
+        self.model = model
+        # norm_input = Input(shape=(self.n_series_len, self.n_series_ft))
+        # lstm_input = self.norm_layer(norm_input)
+        # lstm_layer = LSTM(self.n_layer, activation=self.activation_f, return_sequences=True)(lstm_input)
+        # out = TimeDistributed(Dense(self.n_series_out))(lstm_layer)
+        # self.model = Model(inputs=lstm_input, outputs=out)
+        return self.model
+
+    def TrainModel(self, x_train, y_train, x_val, y_val):
+        """
+        Train the model
+        """
+        if self.model is None:
+            return self.history
+
+        # Compile model
+        self.model.compile(
+            tf.optimizers.Adam(learning_rate=0.001), loss=keras.losses.Huber(), metrics=[keras.metrics.RootMeanSquaredError()]
+        )
+
+        # Fit Model
+        self.history = self.model.fit(
+            x=x_train,
+            y=y_train,
+            epochs=self.epoch,
+            batch_size=self.batch_size,
+            validation_data=(x_val, y_val),
+            shuffle=False,
+            callbacks=self.callbacks,
+        )
+
+        return self.history
+
+    def Predict(self, x_test):
+        """
+        Runs and returns a prediction using the trained model and x_test input.
+
+        Arguments:
+            x_test (ndarray/tensor)     : x test data
+
+        Return
+            prediction output of model
+        """
+        if self.model:
+            self.model.load_weights("../models/multi_layer_lstm_weights.hdf5")
+            return self.model.predict(x_test, verbose=1, batch_size=1, callbacks=self.callbacks)
