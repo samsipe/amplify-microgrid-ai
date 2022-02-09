bda27ebe30392c8c6e8285547f3d1fb89c068c04
Sam Sipe <sam@sipe.io>
2022-02-09 00:45:06 -0500

    add bootstrap, add plot in dash app, power_ss

--- app.py
+++ app.py
@@ -2,36 +2,34 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
-app = Dash(__name__)
-
-colors = {'background': '#111111', 'text': '#7FDBFF'}
+app = Dash(__name__,
+           title='Amplify Microgrid AI',
+           external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame(
-    {
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 9],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-    }
+xy_data = pd.read_csv('./data/xy_data.csv')
+
+fig = px.line(
+    xy_data,
+    x=xy_data.index,
+    labels={"value": "Power (kW)", "index": "Date and Time"},
+    y=["True Power (kW) solar", "True Power (kW) usage"],
+    title="Solar Generation and Building Usage (Power)",
 )
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-fig.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
-
 app.layout = html.Div(
-    style={'backgroundColor': colors['background']},
     children=[
-        html.H1(children='Hello Dash', style={'textAlign': 'center', 'color': colors['text']}),
+        html.H1(children='Amplify Microgrid AI', style={'textAlign': 'center'}),
         html.Div(
             children='''
-        Dash: A web application framework for your data.
+        Power Generation and Usage Predictions
     ''',
-            style={'textAlign': 'center', 'color': colors['text']},
+            style={'textAlign': 'center'},
         ),
         dcc.Graph(id='example-graph', figure=fig),
     ],
