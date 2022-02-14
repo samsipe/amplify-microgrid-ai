89f62753f3aea903f752769fc79263223d0d7496
Sam Sipe <sam@sipe.io>
2022-02-13 20:02:09 -0500

    add model to app

--- app.py
+++ app.py
@@ -1,37 +1,84 @@
 from glob import glob
 
-from clearml import Dataset
+from clearml import Dataset, Model
+from amplify.data import DataSplit
+import tensorflow as tf
+
 from dash import Dash, html, dcc, Input, Output, State
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import numpy as np
 import pandas as pd
 import flask
 
+data_dir = glob(
+    Dataset.get(
+        dataset_name="xy_data",
+        dataset_project="amplify",
+    ).get_local_copy()
+    + "/**"
+)[0]
 
-def add_data():
-    data_dir = glob(
-        Dataset.get(
-            dataset_name="xy_data",
-            dataset_project="amplify",
-        ).get_local_copy()
-        + "/**"
-    )[0]
+xy_data = pd.read_csv(data_dir, index_col=0)
 
-    xy_data = pd.read_csv(data_dir, index_col=0)
+_, _, _, norm_layer = DataSplit(
+    xy_data,
+    series_length=48,
+    stride=1,
+).split_data()
+
+# Define the model
+inputs = tf.keras.layers.Input(shape=(48, 7))
+x = norm_layer(inputs)
+x = tf.keras.layers.LSTM(400, return_sequences=True, dropout=0.2)(x)
+outputs = tf.keras.layers.TimeDistributed(
+    tf.keras.layers.Dense(2, activation="relu", kernel_regularizer="l2")
+)(x)
+model = tf.keras.models.Model(inputs, outputs)
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.MeanSquaredError(),
+    metrics=[tf.keras.metrics.RootMeanSquaredError()],
+)
+
+model_path = Model(model_id="f6b26b93ecc842319d0733711523f22e").get_local_copy()
+
+model.load_weights(model_path)
+
+
+def add_data(xy_data, model):
+    i = np.random.default_rng().integers(0, xy_data.shape[0] - 48)
+    y_preds = model.predict(
+        np.reshape(
+            np.array(
+                xy_data.iloc[i : i + 48].drop(
+                    ["True Power (kW) solar", "True Power (kW) usage"], axis=1
+                )
+            ),
+            (1, 48, 7),
+        )
+    )
 
     fig = px.line(
-        xy_data,
-        x=xy_data.index,
-        y=["True Power (kW) solar", "True Power (kW) usage"],
+        xy_data.iloc[i : i + 48],
+        x=xy_data.iloc[i : i + 48].index,
+        y=[
+            y_preds[0, :, 0].T,
+            y_preds[0, :, 1].T,
+            "True Power (kW) solar",
+            "True Power (kW) usage",
+        ],
         labels={
-            "value": "Power (kW)",
-            "index": "Date and Time",
+            "y": "Power (kW)",
+            "x": "Date and Time",
             "variable": "Features",
         },
     )
 
     fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
     return fig
+
 
 server = flask.Flask(__name__)
 
@@ -103,12 +150,14 @@ dashboard = dbc.Container(
         dcc.Loading(
             html.Div(
                 id="graph_wrapper",
-                children=[dcc.Graph(id="example-graph", figure=add_data())],
+                children=[
+                    dcc.Graph(id="example-graph", figure=add_data(xy_data, model))
+                ],
             )
         ),
         html.Div(
             [
-                dbc.Button("Predict", color="primary"),
+                dbc.Button("Predict", color="primary", href="/"),
             ],
             className="d-grid gap-2 col-6 mx-auto",
         ),
