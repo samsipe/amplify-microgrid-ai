7e2763efc27c76f7ec01095d22071c8f6bf6e21a
Sam Sipe <sam@sipe.io>
2022-02-15 14:36:44 -0500

    update app with forcast and historical data, pytz

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from glob import glob
-
+from datetime import datetime
+import pytz
 from clearml import Dataset, Model
 from tensorflow import keras
 
@@ -13,44 +14,83 @@ from amplify.data import PredictData
 from amplify.data import DataSplit
 from amplify.models import YeetLSTMv2
 
-data_dir = glob(
-    Dataset.get(
-        dataset_name="xy_data",
-        dataset_project="amplify",
-    ).get_local_copy()
-    + "/**"
-)[0]
 
-xy_data = pd.read_csv(data_dir, index_col=0)
+def clearml_setup():
+    # Load the model
+    model = YeetLSTMv2(
+        n_series_len=48,
+        n_series_ft=7,
+        n_series_out=2,
+        n_lstm=400,
+        model_weights_path=Model(
+            model_id="f6b26b93ecc842319d0733711523f22e"
+        ).get_local_copy(),
+        production_mode=True,
+    )
 
-# TODO: remove?
-(x_train, y_train), (x_val, y_val), (x_test, y_test), norm_layer = DataSplit(
-    xy_data,
-    sequence=False,
-).split_data()
+    # Get historical data
+    xy_data = pd.read_csv(
+        glob(
+            Dataset.get(
+                dataset_name="xy_data",
+                dataset_project="amplify",
+            ).get_local_copy()
+            + "/**"
+        )[0],
+        index_col=0,
+    )
+
+    # (x_train, y_train), (x_val, y_val), (x_test, y_test), norm_layer = DataSplit(
+    #     xy_data,
+    # ).split_data()
+
+    # model.evaluate(x_val, y_val, verbose=1)
+    # model.evaluate(x_test, y_test, verbose=1)
+
+    return model, xy_data
 
 
-model_path = Model(model_id="f6b26b93ecc842319d0733711523f22e").get_local_copy()
-#print(model_path)
-model = YeetLSTMv2(n_series_len=48, n_series_ft=7, n_series_out=2, n_lstm=400, model_weights_path=model_path, production_mode=True)
-
-# TODO: remove?
-# model.evaluate(x_val, y_val, verbose=1)
-# model.evaluate(x_test, y_test, verbose=1)
-
-
-def add_data(xy_data, model):
-    i = np.random.default_rng().integers(0, xy_data.shape[0] - 48)
+def historical_data(xy_data, model):
+    """ "This uses validation and test portion of the existing historical data"""
+    i = np.random.default_rng().integers(
+        xy_data.shape[0] - 0.2 * xy_data.shape[0], xy_data.shape[0] - 48
+    )
     y_preds = model.predict(
         np.reshape(
-            np.array(xy_data.iloc[i : i + 48].drop(["True Power (kW) solar", "True Power (kW) usage"], axis=1)),
+            np.array(
+                xy_data.iloc[i : i + 48].drop(
+                    ["True Power (kW) solar", "True Power (kW) usage"], axis=1
+                )
+            ),
             (1, 48, 7),
         )
     )
 
+    fig = px.line(
+        xy_data.iloc[i : i + 48],
+        x=xy_data.iloc[i : i + 48].index,
+        y=[
+            y_preds[0, :, 0].T,
+            y_preds[0, :, 1].T,
+            "True Power (kW) solar",
+            "True Power (kW) usage",
+        ],
+        labels={
+            "y": "Power (kW)",
+            "x": "Date and Time",
+            "variable": "",
+        },
+        color_discrete_sequence=px.colors.qualitative.D3,
+    )
 
-def predict_data(model):
+    fig.update_layout(
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
+    )
+    return fig
 
+
+def forcast_data(model):
+    """This will get data from OpenWeather OnceCall API"""
     preds = PredictData(model).forecast()
     preds.index = preds.index.tz_convert("US/Eastern")
 
@@ -59,15 +99,28 @@ def predict_data(model):
         labels={
             "value": "Power (kW)",
             "dt": "Date and Time",
-            "variable": "Features",
+            "variable": "",
         },
+        color_discrete_sequence=px.colors.qualitative.D3,
     )
-
-    fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+    fig.add_vline(
+        x=datetime.now(pytz.timezone("US/Eastern")),
+        line_width=2,
+        line_color="red",
+        line_dash="dash",
+    )
+    fig.update_layout(
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
+    )
     return fig
 
 
-app = Dash(__name__, title="Amplify Microgrid AI", external_stylesheets=[dbc.themes.ZEPHYR])
+### Dash App Stuff ###
+### -------------- ###
+
+app = Dash(
+    __name__, title="Amplify Microgrid AI", external_stylesheets=[dbc.themes.ZEPHYR]
+)
 
 server = app.server
 
@@ -75,7 +128,7 @@ LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
 nav = dbc.Nav(
     [
-        dbc.NavItem(dbc.NavLink("Real Time", active=True, href="#")),
+        dbc.NavItem(dbc.NavLink("Forcast", active=True, href="#")),
         dbc.NavItem(dbc.NavLink("Historical", active=False, href="#")),
     ],
     pills=True,
@@ -90,7 +143,9 @@ navbar = dbc.Navbar(
                 dbc.Row(
                     [
                         dbc.Col(html.Img(src=LOGO, height="30px")),
-                        dbc.Col(dbc.NavbarBrand("Amplify Microgrid AI", className="ms-2")),
+                        dbc.Col(
+                            dbc.NavbarBrand("Amplify Microgrid AI", className="ms-2")
+                        ),
                     ],
                     align="center",
                     className="g-0",
@@ -123,6 +178,7 @@ def toggle_navbar_collapse(n, is_open):
     return is_open
 
 
+model, xy_data = clearml_setup()
 dashboard = dbc.Container(
     [
         html.Div(
@@ -131,7 +187,10 @@ dashboard = dbc.Container(
         ),
         html.Div(
             id="graph_wrapper",
-            children=[dcc.Graph(id="predict-data", figure=predict_data(model))],
+            children=[
+                dcc.Graph(id="forcast-data", figure=forcast_data(model)),
+                dcc.Graph(id="historical-data", figure=historical_data(xy_data, model)),
+            ],
         ),
         html.Div(
             [
