fix normalization issue (yes sam, this is blacked)

amplify/models.py
@@ -20,6 +20,8 @@ from keras.layers import TimeDistributed
 
 from datetime import datetime
 
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
 
 class IModel:
     """
@@ -32,8 +34,8 @@ class IModel:
         self,
         model_name="unknown",
         model_id=None,
-        log_dir=os.path.join("../", "logs"),
-        model_dir=os.path.join("../", "models"),
+        log_dir=os.path.join(PROJECT_DIR, "logs"),
+        model_dir=os.path.join(PROJECT_DIR, "models"),
         norm_layer=Normalization(),
         lr_factor=0.9,
         lr_patience=3,
@@ -64,12 +66,8 @@ class IModel:
         # files
         self.model_dir = model_dir
         self.log_dir = log_dir
-        self.model_weights_file_name = (
-            self.model_name + "_weights_" + dt_string + ".hdf5"
-        )
-        self.model_weights_file_path = os.path.abspath(
-            os.path.join(model_dir, self.model_weights_file_name)
-        )
+        self.model_weights_file_name = self.model_name + "_weights_" + dt_string + ".hdf5"
+        self.model_weights_file_path = os.path.abspath(os.path.join(model_dir, self.model_weights_file_name))
 
         # model param
         self.norm_layer = norm_layer
@@ -153,9 +151,7 @@ class IModel:
             min_lr=self.min_l_rate,
             verbose=0,
         )
-        self.early_stopping_cb = tf.keras.callbacks.EarlyStopping(
-            monitor="val_loss", mode="min", patience=self.es_patience, verbose=1
-        )
+        self.early_stopping_cb = tf.keras.callbacks.EarlyStopping(monitor="val_loss", mode="min", patience=self.es_patience, verbose=1)
 
     def display_model(self):
         """
@@ -194,9 +190,10 @@ class IModel:
         """
         if not (model_weights_path is None or self.model is None):
             try:
+                self.model_weights_file_path = model_weights_path
                 self.model.load_weights(model_weights_path)
-            except:
-                print("Error: Cannot load model weights!")
+            except Exception as e:
+                print(f"Error: Cannot load model weights! -- {e}")
 
     def predict(self, x_test):
         """
@@ -239,9 +236,7 @@ class SimpleLSTM_1(IModel):
         self.norm_layer = norm_layer
 
         # model callbacks
-        self.reduce_lr = keras.callbacks.LearningRateScheduler(
-            lambda x: 1e-3 * 0.90**x
-        )
+        self.reduce_lr = keras.callbacks.LearningRateScheduler(lambda x: 1e-3 * 0.90 ** x)
 
         self.set_hyper_param()
         self.create_model()
@@ -265,9 +260,7 @@ class SimpleLSTM_1(IModel):
         # seq_input = self.norm_layer(norm_input)
         model = Sequential()
         model.add(Input(shape=(self.n_series_len, self.n_series_ft)))
-        model.add(
-            self.norm_layer
-        )  # , input_shape=(self.n_series_len, self.n_series_ft))
+        model.add(self.norm_layer)  # , input_shape=(self.n_series_len, self.n_series_ft))
         model.add(
             LSTM(
                 self.n_layer,
@@ -359,12 +352,8 @@ class MultiLayerLSTM(IModel):
             min_lr=1e-8,
             verbose=1,
         )
-        self.early_stop = tf.keras.callbacks.EarlyStopping(
-            monitor="val_loss", mode="min", patience=(3 * PATIENCE), verbose=1
-        )
-        self.tensorboard_logs = tf.keras.callbacks.TensorBoard(
-            "../logs/", histogram_freq=1
-        )
+        self.early_stop = tf.keras.callbacks.EarlyStopping(monitor="val_loss", mode="min", patience=(3 * PATIENCE), verbose=1)
+        self.tensorboard_logs = tf.keras.callbacks.TensorBoard("../logs/", histogram_freq=1)
         self.callbacks = [
             self.model_cp,
             self.reduce_lr,
@@ -394,9 +383,7 @@ class MultiLayerLSTM(IModel):
         # seq_input = self.norm_layer(norm_input)
         model = Sequential()
         model.add(Input(shape=(self.n_series_len, self.n_series_ft)))
-        model.add(
-            self.norm_layer
-        )  # , input_shape=(self.n_series_len, self.n_series_ft))
+        model.add(self.norm_layer)  # , input_shape=(self.n_series_len, self.n_series_ft))
         model.add(Dropout(rate=self.dropout_rate))
         model.add(Dense(self.n_series_ft, activation="relu"))
         model.add(
@@ -455,9 +442,7 @@ class MultiLayerLSTM(IModel):
         """
         if self.model:
             self.model.load_weights("../models/multi_layer_lstm_weights.hdf5")
-            return self.model.predict(
-                x_test, verbose=1, batch_size=1, callbacks=self.callbacks
-            )
+            return self.model.predict(x_test, verbose=1, batch_size=1, callbacks=self.callbacks)
 
 
 class YeetLSTMv1(IModel):
@@ -510,9 +495,7 @@ class YeetLSTMv1(IModel):
 
         # hyper param
         # *NOTE: IModel has factor, patience, l_rate, dropout, batch, epoch
-        self.es_patience = (
-            self.es_patience if self.es_patience else patience_c * self.batch
-        )
+        self.es_patience = self.es_patience if self.es_patience else patience_c * self.batch
 
         # callbacks
         self.setup_callbacks()
@@ -542,12 +525,8 @@ class YeetLSTMv1(IModel):
 
         decoder_inputs = RepeatVector(self.n_series_len)(encoder_outputs1[0])
 
-        decoder_l1 = LSTM(self.n_lstm_layers, return_sequences=True)(
-            decoder_inputs, initial_state=encoder_states1
-        )
-        decoder_outputs1 = TimeDistributed(
-            Dense(self.n_series_out, activation=self.activation_fn)
-        )(decoder_l1)
+        decoder_l1 = LSTM(self.n_lstm_layers, return_sequences=True)(decoder_inputs, initial_state=encoder_states1)
+        decoder_outputs1 = TimeDistributed(Dense(self.n_series_out, activation=self.activation_fn))(decoder_l1)
 
         self.model = Model(norm_inputs, decoder_outputs1)
         return self.model
@@ -591,9 +570,7 @@ class YeetLSTMv1(IModel):
         """
         if self.model:
             self.model.load_weights(self.model_weights_file_path)
-            return self.model.predict(
-                x_test, verbose=1, batch_size=1, callbacks=self.callbacks
-            )
+            return self.model.predict(x_test, verbose=1, batch_size=1, callbacks=self.callbacks)
 
 
 class YeetLSTMv2(IModel):
@@ -603,7 +580,7 @@ class YeetLSTMv2(IModel):
 
     def __init__(
         self,
-        norm_layer: Normalization = Normalization(mean=0.0, variance=1.0),
+        norm_layer: Normalization = Normalization(axis=-1),
         n_series_len=48,
         n_series_ft=7,
         n_series_out=1,
@@ -649,9 +626,7 @@ class YeetLSTMv2(IModel):
 
         # hyper param
         # *NOTE: IModel has factor, patience, l_rate, dropout, batch, epoch
-        self.es_patience = (
-            self.es_patience if self.es_patience else patience_c * self.batch
-        )
+        self.es_patience = self.es_patience if self.es_patience else patience_c * self.batch
 
         # callbacks
         self.setup_callbacks()
@@ -724,10 +699,8 @@ class YeetLSTMv2(IModel):
             prediction output of model
         """
         if self.model:
-            self.model.load_weights(self.model_weights_file_path)
-            return self.model.predict(
-                x_test, verbose=1, batch_size=1, callbacks=self.callbacks
-            )
+            self.load_weights(self.model_weights_file_path)
+            return self.model.predict(x_test, verbose=1, batch_size=1, callbacks=self.callbacks)
 
     def scheduler(self, epoch):
         """

app.py
@@ -23,33 +23,33 @@ data_dir = glob(
 
 xy_data = pd.read_csv(data_dir, index_col=0)
 
+#TODO: remove?
 (x_train, y_train), (x_val, y_val), (x_test, y_test), norm_layer = DataSplit(
     xy_data,
     sequence=False,
 ).split_data()
 
-# Define the model
-inputs = keras.layers.Input(shape=(48, 7))
-x = keras.layers.Normalization()(inputs)
-x = keras.layers.LSTM(400, return_sequences=True, dropout=0.2)(x)
-outputs = keras.layers.TimeDistributed(
-    keras.layers.Dense(2, activation="relu", kernel_regularizer="l2")
-)(x)
-model = keras.models.Model(inputs, outputs)
-
-model.compile(
-    # optimizer=keras.optimizers.Adam(),
-    # loss=keras.losses.MeanSquaredError(),
-    # metrics=[keras.metrics.RootMeanSquaredError()],
-)
 
 model_path = Model(model_id="f6b26b93ecc842319d0733711523f22e").get_local_copy()
-model.load_weights(model_path)
-# model = YeetLSTMv2(n_series_len=48, n_series_ft=7, n_series_out=2, n_lstm=400, model_weights_path=model_path)
+print(model_path)
+model = YeetLSTMv2(n_series_len=48, n_series_ft=7, n_series_out=2, n_lstm=400, model_weights_path=model_path)
 
+#TODO: remove?
 # model.evaluate(x_val, y_val, verbose=1)
 # model.evaluate(x_test, y_test, verbose=1)
 
+def add_data(xy_data, model):
+    i = np.random.default_rng().integers(0, xy_data.shape[0] - 48)
+    y_preds = model.predict(
+        np.reshape(
+            np.array(
+                xy_data.iloc[i : i + 48].drop(
+                    ["True Power (kW) solar", "True Power (kW) usage"], axis=1
+                )
+            ),
+            (1, 48, 7),
+        )
+    )
 
 def predict_data(model):
 
@@ -94,9 +94,7 @@ navbar = dbc.Navbar(
                 dbc.Row(
                     [
                         dbc.Col(html.Img(src=LOGO, height="30px")),
-                        dbc.Col(
-                            dbc.NavbarBrand("Amplify Microgrid AI", className="ms-2")
-                        ),
+                        dbc.Col(dbc.NavbarBrand("Amplify Microgrid AI", className="ms-2")),
                     ],
                     align="center",
                     className="g-0",