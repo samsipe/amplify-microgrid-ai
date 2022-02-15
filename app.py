c6a57f2682960e14e795d5f149b39cfddc233a51
Sam Sipe <sam@sipe.io>
2022-02-15 01:14:16 -0500

    working app, bump v0.2.0

--- app.py
+++ app.py
@@ -1,76 +1,66 @@
 from glob import glob
 
 from clearml import Dataset, Model
-from amplify.data import DataSplit
-from amplify.models import YeetLSTMv2
-import tensorflow as tf
+from tensorflow import keras
 
 from dash import Dash, html, dcc, Input, Output, State
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import numpy as np
 import pandas as pd
-import flask
 
-#data_dir = glob(
-#    Dataset.get(
-#        dataset_name="xy_data",
-#        dataset_project="amplify",
-#    ).get_local_copy()
-#    + "/**"
-#)[0]
+from amplify.data import PredictData
+from amplify.data import DataSplit
+from amplify.models import YeetLSTMv2
 
-#xy_data = pd.read_csv(data_dir, index_col=0)
+data_dir = glob(
+    Dataset.get(
+        dataset_name="xy_data",
+        dataset_project="amplify",
+    ).get_local_copy()
+    + "/**"
+)[0]
 
-#_, _, _, norm_layer = DataSplit(
-#    xy_data,
-#    series_length=48,
-#    stride=1,
-#).split_data()
+xy_data = pd.read_csv(data_dir, index_col=0)
+
+(x_train, y_train), (x_val, y_val), (x_test, y_test), norm_layer = DataSplit(
+    xy_data,
+    sequence=False,
+).split_data()
 
 # Define the model
-#inputs = tf.keras.layers.Input(shape=(48, 7))
-#x = norm_layer(inputs)
-#x = tf.keras.layers.LSTM(400, return_sequences=True, dropout=0.2)(x)
-#outputs = tf.keras.layers.TimeDistributed(
-#    tf.keras.layers.Dense(2, activation="relu", kernel_regularizer="l2")
-#)(x)
-#model = tf.keras.models.Model(inputs, outputs)
-#
-#model.compile(
-#    optimizer=tf.keras.optimizers.Adam(),
-#    loss=tf.keras.losses.MeanSquaredError(),
-#    metrics=[tf.keras.metrics.RootMeanSquaredError()],
-#)
+inputs = keras.layers.Input(shape=(48, 7))
+x = keras.layers.Normalization()(inputs)
+x = keras.layers.LSTM(400, return_sequences=True, dropout=0.2)(x)
+outputs = keras.layers.TimeDistributed(
+    keras.layers.Dense(2, activation="relu", kernel_regularizer="l2")
+)(x)
+model = keras.models.Model(inputs, outputs)
+
+model.compile(
+    # optimizer=keras.optimizers.Adam(),
+    # loss=keras.losses.MeanSquaredError(),
+    # metrics=[keras.metrics.RootMeanSquaredError()],
+)
+
 model_path = Model(model_id="f6b26b93ecc842319d0733711523f22e").get_local_copy()
-model = YeetLSTMv2(n_series_len=48, n_series_ft=7, n_series_out=2, n_lstm=200, model_weights_path=model_path)
+model.load_weights(model_path)
+# model = YeetLSTMv2(n_series_len=48, n_series_ft=7, n_series_out=2, n_lstm=400, model_weights_path=model_path)
+
+# model.evaluate(x_val, y_val, verbose=1)
+# model.evaluate(x_test, y_test, verbose=1)
 
 
-def add_data(xy_data, model):
-    i = np.random.default_rng().integers(0, xy_data.shape[0] - 48)
-    y_preds = model.predict(
-        np.reshape(
-            np.array(
-                xy_data.iloc[i : i + 48].drop(
-                    ["True Power (kW) solar", "True Power (kW) usage"], axis=1
-                )
-            ),
-            (1, 48, 7),
-        )
-    )
+def predict_data(model):
+
+    preds = PredictData(model).forecast()
+    preds.index = preds.index.tz_convert("US/Eastern")
 
     fig = px.line(
-        xy_data.iloc[i : i + 48],
-        x=xy_data.iloc[i : i + 48].index,
-        y=[
-            y_preds[0, :, 0].T,
-            y_preds[0, :, 1].T,
-            "True Power (kW) solar",
-            "True Power (kW) usage",
-        ],
+        preds,
         labels={
-            "y": "Power (kW)",
-            "x": "Date and Time",
+            "value": "Power (kW)",
+            "dt": "Date and Time",
             "variable": "Features",
         },
     )
@@ -79,20 +69,18 @@ def add_data(xy_data, model):
     return fig
 
 
-server = flask.Flask(__name__)
-
 app = Dash(
-    __name__,
-    title="Amplify Microgrid AI",
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-    server=server,
+    __name__, title="Amplify Microgrid AI", external_stylesheets=[dbc.themes.ZEPHYR]
 )
+
+server = app.server
 
 LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
 nav = dbc.Nav(
     [
-        dbc.NavItem(dbc.NavLink("Prediction", active=True, href="#")),
+        dbc.NavItem(dbc.NavLink("Real Time", active=True, href="#")),
+        dbc.NavItem(dbc.NavLink("Historical", active=False, href="#")),
     ],
     pills=True,
     className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
@@ -142,17 +130,14 @@ def toggle_navbar_collapse(n, is_open):
 
 
 dashboard = dbc.Container(
-    children=[
-        html.H4(
-            children="Power Generation and Usage Predictions",
+    [
+        html.Div(
+            html.H4("Power Generation and Usage Predictions"),
+            className="mt-5 mx-auto",
         ),
-        dcc.Loading(
-            html.Div(
-                id="graph_wrapper",
-                children=[
-                    dcc.Graph(id="example-graph", figure=add_data(xy_data, model))
-                ],
-            )
+        html.Div(
+            id="graph_wrapper",
+            children=[dcc.Graph(id="predict-data", figure=predict_data(model))],
         ),
         html.Div(
             [
@@ -163,8 +148,32 @@ dashboard = dbc.Container(
     ]
 )
 
-app.layout = html.Div([navbar, dashboard])
+footer = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.Row(
+                dbc.Col(html.Footer(html.P("© 2022 Amplify", className="ms-2"))),
+                align="center",
+                className="g-0",
+            )
+        ]
+    ),
+    color="light",
+    className="fixed-bottom",
+)
 
+
+def serve_layout():
+    return html.Div(
+        [
+            navbar,
+            dashboard,
+            footer,
+        ]
+    )
+
+
+app.layout = serve_layout
 
 if __name__ == "__main__":
     app.run_server(debug=True)
